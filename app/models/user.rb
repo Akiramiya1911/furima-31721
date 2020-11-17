251736b384503999f086276b756108fe62fa9242
Akiramiya1911 <akiramiya1911@gmail.com>
2020-11-17 13:57:45 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  PASSWORD_REGEX = /\A(?=[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
+    validates :last_name_reading
+    validates :first_name_reading
+  end
+
+  validates :birth_date, presence: true
 end
