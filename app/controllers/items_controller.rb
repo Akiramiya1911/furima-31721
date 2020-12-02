aefca90452821288a936aa30e83087ca70dee769
Akiramiya1911 <akiramiya1911@gmail.com>
2020-12-02 11:18:11 +0900

    不備の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path if current_user.id != @item.user_id
+    @item_purchase = @item.item_purchase
+    redirect_to root_path if current_user.id != @item.user_id || @item_purchase
   end
 
   def update
